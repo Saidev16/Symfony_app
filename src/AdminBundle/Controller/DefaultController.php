1aa2ae99a89a23ec8f1a0d74a86b1b0bcd984470
boutouditsaid@gmail.com <boutouditsaid@gmail.com>
2021-03-24 17:22:29 +0100

    Send mail & Contact page

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -4,6 +4,10 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,5 +17,36 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('AdminBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/contact", name="contact_page" )
+     * @Method ({ "GET", "POST" })
+     */
+    public function contact( Request $request  ){
+        $form = $this->createFormBuilder()
+            ->add('from')
+            ->add('subject')
+            ->add('body', TextareaType::class)
+            ->add('Send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() ){
+            //Traitement de mail
+            $data = $form->getData();
+
+            $message = (new \Swift_Message($data['subject']))
+            ->setFrom($data['from'])
+            ->setTo('saidboutoudit@ewa.ma')
+            ->setBody($data['body'], 'text/plain');
+
+            $this->get('mailer')->send($message);
+            
+        }
+        
+        return $this->render('default/contact.html.twig', ['form' => $form->createView()]);
     }
 }
